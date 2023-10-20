285b8d196a7f9b6d42bbae41595b7bdaf0ebddd7
chirag gaur <chiragwalker@gmail.com>
2023-10-20 10:03:00 +0530

    done

--- block-BRaaaa/code/script.js
+++ block-BRaaaa/code/script.js
@@ -1,33 +1,45 @@
 // selected html elements
-let box = document.querySelector("#input_movie");
+let input = document.querySelector("#input_movie");
 let list = document.querySelector(".movie_list");
-
 let allMovies = [];
-
-box.addEventListener("keyup", (event) => {
+input.addEventListener("keyup", (event) => {
+  // allMovies = [];
   if (event.keyCode === 13) {
     allMovies.push({
       name: event.target.value,
       watched: false,
     });
     event.target.value = "";
-    createMovieUI();
+    createMovieUI(allMovies, list);
   }
 });
 
-function createMovieUI() {
-  allMovies.forEach((elm) => {
+function handleChange(event) {
+  let id = event.target.id;
+  allMovies[id].watched = !allMovies[id].watched;
+  createMovieUI(allMovies, list);
+}
+
+function createMovieUI(data, root) {
+  root.innerHTML = "";
+  data.forEach((elm, i) => {
     let li = document.createElement("li");
-    li.innerHTML = elm.name;
+    li.classList.add("movie_list");
+    let button = document.createElement("button");
+    button.id = i;
+    button.classList.add("movie_status");
+    button.innerText = elm.watched ? "watched" : "To Watch";
+    button.addEventListener("click", handleChange);
+    let label = document.createElement("label");
+    label.for = i;
+    label.classList.add("movie_name");
+    label.innerText = elm.name;
+    label.append(button);
+    li.append(label);
     list.append(li);
   });
 }
 
-// delete on cross from movie list and then create UI
+createMovieUI(allMovies, list);
 
-function deleteMovie(event) {
-  let id = event.target.dataset.id;
-  allMovies.splice(id, 1);
-  createMovieUI();
-}
-// error in event listener
+// status
