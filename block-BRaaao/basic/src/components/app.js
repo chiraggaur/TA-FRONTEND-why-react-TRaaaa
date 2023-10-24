4c7c7382f4922eef889faca29d4fb858c9781a89
chirag gaur <chiragwalker@gmail.com>
2023-10-24 15:34:49 +0530

    done

---
+++ block-BRaaao/basic/src/components/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+import Hero from "./Hero";
+import Articles from "./Article";
+import foot_nav from "./Footer";
+// component
+
+function App() {
+  return [<Header />, <Hero />, <Articles />, <foot_nav />];
+}
+
+export default App;
